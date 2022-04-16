40b25fbbfbe2e6db22bb9d4b7887471277518928
abdelrahman-coder <63162992+abdelrahman-coder@users.noreply.github.com>
2022-04-16 12:54:29 +0200

    Initial commit

---
+++ src/com/sig/model/InvLine.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sig.model;
+
+/**
+ *
+ * @author abdel
+ */
+public class InvLine {
+    
+private String itemName;
+private double itemPrice;
+private int itemCount;
+private double lineTotal;
+private InvHead header;
+
+    public InvLine(String itemName, double itemPrice, int itemCount, InvHead header) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemCount = itemCount;
+        this.header = header;
+        this.setLineTotal(this.itemCount*this.itemPrice);
+    }
+
+     
+    public InvHead getHeader() {
+        return header;
+    }
+
+    public void setHeader(InvHead header) {
+        this.header = header;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public double getLineTotal(){
+        return lineTotal;
+    }
+
+    private void setLineTotal(double lineTotal) {
+this.lineTotal = lineTotal;
+  }
+     public String getDataAsCSV() {
+        return "" + getHeader().getInvNum() + "," + getItemName() + "," + getItemPrice() + "," + getItemCount();
+    }
+}
